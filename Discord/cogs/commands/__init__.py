4167d599493ce77fdc0e0fd9d7f305dc645a3ec3
andrejklol <73305572+andrejklol@users.noreply.github.com>
2026-04-12 23:33:13 -0500

    Add permission checks and levels to init.py

--- Discord/cogs/commands/__init__.py
+++ Discord/cogs/commands/__init__.py
@@ -4,6 +4,16 @@ from .consigliere_commands import ConsigliereCommands
 from .capo_commands import CapoCommands
 from .general_commands import GeneralCommands
 
+from .permissions import (
+    check_level,
+    LEVEL_OWNER,
+    LEVEL_UNDERBOSS,
+    LEVEL_CONSIGLIERE,
+    LEVEL_CAPO,
+    LEVEL_SOLDIER,
+    LEVEL_USER,
+)
+
 __all__ = [
     "OwnerCommands",
     "UnderbossCommands",
@@ -11,4 +21,12 @@ __all__ = [
     "CapoCommands",
     "GeneralCommands",
     "perform_command_sync",
+
+    "check_level",
+    "LEVEL_OWNER",
+    "LEVEL_UNDERBOSS",
+    "LEVEL_CONSIGLIERE",
+    "LEVEL_CAPO",
+    "LEVEL_SOLDIER",
+    "LEVEL_USER",
 ]
