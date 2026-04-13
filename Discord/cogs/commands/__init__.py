bfd8eb40f0c397a2dfbef5073b63815ece0ab177
andrejklol <73305572+andrejklol@users.noreply.github.com>
2026-04-12 23:02:43 -0500

    Import command classes and define __all__

--- Discord/cogs/commands/__init__.py
+++ Discord/cogs/commands/__init__.py
@@ -1,1 +1,14 @@
+from .owner_commands import OwnerCommands, perform_command_sync
+from .underboss_commands import UnderbossCommands
+from .consigliere_commands import ConsigliereCommands
+from .capo_commands import CapoCommands
+from .general_commands import GeneralCommands
 
+__all__ = [
+    "OwnerCommands",
+    "UnderbossCommands",
+    "ConsigliereCommands",
+    "CapoCommands",
+    "GeneralCommands",
+    "perform_command_sync",
+]
